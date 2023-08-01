fe8975472f8036c27fb06099b51ed62b163ab683
jonarosero <jars9605@hotmail.com>
2023-07-31 20:40:30 -0500

    all services

---
+++ product/src/main/java/tec/utpl/store/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package tec.utpl.store.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
---
+++ shopping/src/main/java/tec/utpl/store/shopping/ShoppingApplication.java
@@ -0,0 +1,13 @@
+package tec.utpl.store.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingApplication.class, args);
+	}
+
+}
