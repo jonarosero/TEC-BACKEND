d42972c8d1dc1bf7191a14db810edcba3010125b
jonarosero <jars9605@hotmail.com>
2023-07-31 21:30:19 -0500

    feat: config server

--- config-service/src/main/java/tec/utpl/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/tec/utpl/configservice/ConfigServiceApplication.java
@@ -2,8 +2,9 @@ package tec.utpl.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
 
